db24605c36ff0a7529da18c309119c79c9a3992b
Yoones Mahdian <mahdianyoones@gmail.com>
2020-06-03 19:50:32 +0430

    Changes the factors variable type to set for duplicates prevention

--- NumFactors.py
+++ NumFactors.py
@@ -2,16 +2,15 @@ def getFactors(number):
     divisor = 1
     quotient = number # just to make it bigger than the divisor for the while below to work
 
-    factors = [] # list, ordered
+    factors = set() # list, ordered
 
     while quotient > divisor:
         if number % divisor == 0:
             quotient = number // divisor
-            factors.append(quotient)
-            factors.append(divisor)
+            factors.add(quotient)
+            factors.add(divisor)
         divisor += 1
 
-    factors.sort()
     return factors
 
 if __name__ == "__main__":
